9d60150490fd0b21eba95bd316dfb4a0a6c1d8bf
Flavien <flavien.hardy.17@ucl.ac.uk>
2018-11-08 13:27:47 +0000

    Minor visual changes to SystemParameters file

--- DipoleCAN_Numerical/SystemParameters.m
+++ DipoleCAN_Numerical/SystemParameters.m
@@ -1,17 +1,17 @@
 function SystemParameters = SystemParameters(Planet)
 
     if Planet == "Saturn"
-        
+
         % Planet and Pressure Balance at SATURN
-    	SystemParameters.beta = 2;               % Plasma Beta (particle pressure / magnetic pressure)
-    	SystemParameters.Bp = 20000*10^(-9);     % Equatorial field, T
-        SystemParameters.Rp = 60280*10^3;        % Planet radius, m
+    	  SystemParameters.beta = 2;             % Plasma Beta (particle pressure / magnetic pressure)
+    	  SystemParameters.Bp = 20000*10^(-9);   % Equatorial field, T
+        SystemParameters.Rp = 60280*10^3;      % Planet radius, m
         mu0 = 4*pi*10^(-7);
         Pswnpa = 0.02;
         SystemParameters.b0 = sqrt(2*mu0*Pswnpa*10^(-9));                                                      % Field scale
         SystemParameters.r0 = (2*SystemParameters.Bp*SystemParameters.Rp^3/SystemParameters.b0)^(1/3);         % Distance scale
         SystemParameters.M = SystemParameters.Bp*SystemParameters.Rp^3 / (SystemParameters.b0*SystemParameters.r0^3);
-        
+
         % CAN Disk Parameters (Bunce2007)
         SystemParameters.mu0I = ( -60.4*10^(-9) /SystemParameters.b0 ) ;      % (mu0 I0 = 60.4 nT)
         SystemParameters.a = 8;      % Inner radius of the current distribution (in Rp)
@@ -20,7 +20,7 @@ function SystemParameters = SystemParameters(Planet)
         SystemParameters.CAN_DiskParameters = [SystemParameters.mu0I SystemParameters.a SystemParameters.b SystemParameters.D];
 
     elseif Planet == "Jupiter"
-        
+
         % Planet and Pressure Balance at JUPITER
         SystemParameters.beta = 15;              % Plasma Beta (particle pressure / magnetic pressure)
         SystemParameters.Bp = 776.6*10^(-6);     % Equatorial field, T
@@ -39,8 +39,8 @@ function SystemParameters = SystemParameters(Planet)
         SystemParameters.a = 5;      % Inner radius of the current distribution (in Rp)
         SystemParameters.b = 30;   % Outer radius of the current distribution (in Rp)
         SystemParameters.D = 2.5;      % Disk Semi-thickness (in Rp)
-        SystemParameters.CAN_DiskParameters = [SystemParameters.mu0I SystemParameters.a SystemParameters.b SystemParameters.D];  
-        
+        SystemParameters.CAN_DiskParameters = [SystemParameters.mu0I SystemParameters.a SystemParameters.b SystemParameters.D];
+
     end
 
 end
